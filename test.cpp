eb57256b49c00b289d4cb5fc3900a5e51feadcb3
dengbei-victor <971817049@qq.com>
2021-05-24 14:01:08 +0800

    5.24

--- test.cpp
+++ test.cpp
@@ -1,29 +1,33 @@
 #include <iostream>
-#include <vector>
-#include <string>
-
+#include <cmath>
 using namespace std;
 
-// 使用三点求面积
-int CalTriEdgeSize(int []);
-int CalTriArea(int, int, int);
+// 使用三点求面积  Sabc=1/2|x1y2-x2y1|
+int CalTriArea(int[][2]);
 
-int CalTriEdgeSize(int a[])
+int CalTriArea(int a[][2])
 {
-    
+    int x1,y1,x2,y2;
+    int S;
+    x1=a[0][0]-a[1][0];
+    x2=a[2][0]-a[1][0];
+    y1=a[0][1]-a[1][1];
+    y2=a[2][1]-a[1][1];
+    S=abs(x1*y2-x2*y1)/2;
+    return S;
 }
 
 int main()
 {
-    
-    int a[3][3];
-    for (int i=0;i<3;i++)
+    int a[3][2];
+    for (int i = 0; i < 3; i++)
     {
-        for (int j = 0; j < 3; j++)
+        for (int j=0; j<2; j++)
         {
-            cin >> a[i];
+            cin >> a[i][j];
         }
         
     }
-    cout << endl;
+    cout << CalTriArea(a) <<endl;
+    return 0;
 }
